56571068b28e31b8b2861a5d6a2fb96cb867371c
benjaminjkamp <mail@benjaminjkamp.com>
2019-05-08 13:55:59 -0500

    mini-capstone add route for all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,18 @@
 class Api::ProductsController < ApplicationController
+
+  def product_listing
+    @products = Product.all
+    @phones = []
+    @products.each do |product|
+      @phones << product.name
+      @phones << product.description
+      @phones << product.price
+      @phones << product.image_url
+    end
+  
+
+    # @products = Product.all
+    render "products.json.jbuilder"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-Product.create(name: "Pixel 2XL", price: 900, image_url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcScdgBef17ZuhxgfvuXISI8rnTbqaj--dtRgzDQkAqlFjX9Db85DQM&usqp=CAc", description: "2nd generation XL phone made by Google")
+Product.create(name: "Pixel 3XL", price: 899, image_url: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTF9CzFhkLuKL_dyXVnXdx29Ni7ztiDk5d2QuMuAkTTRoI_e9JDpw&usqp=CAc", description: "3rd generation XL phone made by Google")
+Product.create(name: "Pixel 3a", price: 399, image_url: "https://www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge_wm_brw/public/article_images/2019/05/pixel-3a-xl-preview-12.jpg?itok=oSN2poHI", description: "3rd generation budget version phone made by Google")
 
