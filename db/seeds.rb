ee6d0ada5c7384032200784ef6cae50fd5fe0ecb
benjaminjkamp <mail@benjaminjkamp.com>
2019-05-08 13:12:44 -0500

    mini-capstone add product to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Pixel 2XL", price: 900, image_url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcScdgBef17ZuhxgfvuXISI8rnTbqaj--dtRgzDQkAqlFjX9Db85DQM&usqp=CAc", description: "2nd generation XL phone made by Google")
+
