0b44b498a27fa9193a671aafb4396b1c5eebf23b
benjaminjkamp <mail@benjaminjkamp.com>
2019-06-01 00:35:55 -0500

    adds public pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,4 +29,27 @@ class ProductsController < ApplicationController
     render 'show.html.erb'
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.name = params[:name]
+    @product.description = params[:description]
+    @product.price = params[:price]    
+    if @product.save
+      redirect_to "/products/#{@product.id}"
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,15 +8,12 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders, through: :carted_products
   has_many :users, through: :carted_products
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
 
-  # validates :name, :price, presence: true
-  # validates :name, uniqueness: true
-  # validates :price, numericality: true
-  # validates :price, numericality: { greater_than: 0 }
-  # validates :description, length: { in: 10..500 }
+  validates :name, :price, presence: true
+  validates :name, uniqueness: true
+  validates :price, numericality: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { in: 10..500 }
 
   def is_discounted?
     price < 10
